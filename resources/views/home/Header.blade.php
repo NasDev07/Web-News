76792a4fbceec5700180ce4ca26f20ff2da5282b
nasruddin <nasruddin20182019@gmail.com>
2023-03-17 20:02:39 +0700

    blog news

--- resources/views/home/Header.blade.php
+++ resources/views/home/Header.blade.php
@@ -28,17 +28,17 @@
             <a href="#" class="mx-2"><span class="bi-twitter"></span></a>
             <a href="#" class="mx-2"><span class="bi-instagram"></span></a>
 
-            <a href="#" class="mx-2 js-search-open"><span class="bi-search"></span></a>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-
             <!-- ======= Search Form ======= -->
-            <div class="search-form-wrap js-search-form-wrap">
+            {{-- <a href="#" class="mx-2 js-search-open"><span class="bi-search"></span></a>
+            <i class="bi bi-list mobile-nav-toggle"></i> --}}
+            {{-- <div class="search-form-wrap js-search-form-wrap">
                 <form action="search-result.html" class="search-form">
                     <span class="icon bi-search"></span>
                     <input type="text" placeholder="Search" class="form-control">
                     <button class="btn js-search-close"><span class="bi-x"></span></button>
                 </form>
-            </div><!-- End Search Form -->
+            </div> --}}
+            <!-- End Search Form -->
 
         </div>
 
